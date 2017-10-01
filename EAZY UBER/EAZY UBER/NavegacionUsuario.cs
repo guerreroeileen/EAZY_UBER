05c5896bf69f7df9d2c4b5bb6e5d8ba482a5c91b
Daniel Eduardo Quintero Correa <danielquin1997@gmail.com>
2017-09-30 21:28:00 -0500

    Se crea un form para la navegación del usuario

---
+++ EAZY UBER/EAZY UBER/NavegacionUsuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAZY_UBER
+{
+    public partial class NavegacionUsuario : Form
+    {
+        public NavegacionUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
