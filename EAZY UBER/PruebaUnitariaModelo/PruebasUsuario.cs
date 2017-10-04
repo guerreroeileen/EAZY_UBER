1b38a2e1f752eb42b60da9c14cf9da007f9ef2b6
Juan <Juan>
2017-10-04 09:20:19 -0500

    Agrega proyecto de pruebas unitarias

---
+++ EAZY UBER/PruebaUnitariaModelo/PruebasUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebaUnitariaModelo
+{
+    [TestClass]
+    public class PruebasUsuario
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
