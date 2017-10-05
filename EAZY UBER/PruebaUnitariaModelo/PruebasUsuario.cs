63bf57257458b2a8c793ac8c892c6b070c475f98
Juan <Juan>
2017-10-05 08:35:39 -0500

    Terminado el setup2 de las pruebas unitarias de Usuario.

--- EAZY UBER/PruebaUnitariaModelo/PruebasUsuario.cs
+++ EAZY UBER/PruebaUnitariaModelo/PruebasUsuario.cs
@@ -33,6 +33,15 @@ namespace PruebaUnitariaModelo
             vito.registrarVehiculo("GFB469", "Azul", "Cayenne", "");
             vito.registrarVehiculo("NGJ985", "Negro", "Cadilac", "");
             vito.registrarRuta("El peaje", new Tuple<double, double>(95.544, 56.2255), new Tuple<double, double>(105.658, 86.485), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(98.544, 66.2255), new Tuple<double, double>(102.65, 76.2255), new Tuple<double, double>(104.695, 82.225) },"");
+
+            Usuario michael = sist.darUsuario("3068455");
+            michael.registrarVehiculo("GFB469", "Azul", "Cayenne", "");
+            michael.registrarVehiculo("NGJ985", "Negro", "Cadilac", "");
+
+            Usuario vincent = sist.darUsuario("33265558");
+            vincent.registrarRuta("El teatro", new Tuple<double, double>(45.365, 69.265), new Tuple<double, double>(98.254, 35.365), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(52.698, 59.333), new Tuple<double, double>(91.365, 42.365) }, "Buena la Obra, triste el final");
+            vincent.registrarRuta("El teatro", new Tuple<double, double>(33.254, 71.951), new Tuple<double, double>(94.365, 38.985), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(48.658, 55.355), new Tuple<double, double>(89.451, 38.923) }, "");
+
         }
 
         [TestMethod]
