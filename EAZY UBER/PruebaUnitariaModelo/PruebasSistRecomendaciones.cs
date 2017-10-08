9724508dff4d604241ed507e80eddfafd314ac81
Juan <Juan>
2017-10-08 12:39:38 -0500

    Modificaciones setup 2, falta revizar el orden del diccionario, en el metodo se encuentra el orden de acuerdo al escenario

--- EAZY UBER/PruebaUnitariaModelo/PruebasSistRecomendaciones.cs
+++ EAZY UBER/PruebaUnitariaModelo/PruebasSistRecomendaciones.cs
@@ -29,20 +29,21 @@ namespace PruebaUnitariaModelo
             mundito.registrarUsuario("vito", "corleone", "30155844", "del0al9", "padrino@correo.mafia.com", "", new Tuple<double, double>(1.2222, 1.6588));
             mundito.registrarUsuario("michael", "corleone", "3068455", "del10al20", "padrino2@correo.mafia.com", "", new Tuple<double, double>(1.2222, 1.6588));
             mundito.registrarUsuario("fredo", "corleone", "3095488541", "del20al30", "casipadrino@correo.mafia.com", "", new Tuple<double, double>(1.2222, 1.6588));
-            mundito.registrarUsuario("vincent", "corleone", "33265558", "de40a50", "falsopadrino@correo.mafia.com", "", new Tuple<double, double>(1.2222, 1.6588));
+            mundito.registrarUsuario("vincent", "corleone", "33265558", "de40a50", "falsopadrino@correo.mafia.com", "", new Tuple<double, double>(1,1));
 
             Usuario vito = mundito.darUsuario("30155844");
             vito.registrarVehiculo("GFB469", "Azul", "Cayenne", "");
             vito.registrarVehiculo("NGJ985", "Negro", "Cadilac", "");
-            vito.registrarRuta("El peaje", new Tuple<double, double>(95.544, 56.2255), new Tuple<double, double>(105.658, 86.485), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(98.544, 66.2255), new Tuple<double, double>(102.65, 76.2255), new Tuple<double, double>(104.695, 82.225) }, "");
+            vito.registrarRuta("El peaje", new Tuple<double, double>(-2,0.5), new Tuple<double, double>(3,0.5), null, "");
 
             Usuario michael = mundito.darUsuario("3068455");
             michael.registrarVehiculo("MKL598", "Blanco", "Toyota", "");
             michael.registrarVehiculo("CRL656", "Gris", "Ford", "");
+            michael.registrarRuta("La calle saza", new Tuple<double, double>(1,3), new Tuple<double, double>(3,1), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(1,2), new Tuple<double, double>(2,1) }, "");
 
             Usuario vincent = mundito.darUsuario("33265558");
-            vincent.registrarRuta("El teatro", new Tuple<double, double>(45.365, 69.265), new Tuple<double, double>(98.254, 35.365), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(52.698, 59.333), new Tuple<double, double>(91.365, 42.365) }, "Buena la Obra, triste el final");
-            vincent.registrarRuta("La calles saza", new Tuple<double, double>(33.254, 71.951), new Tuple<double, double>(94.365, 38.985), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(48.658, 55.355), new Tuple<double, double>(89.451, 38.923) }, "");
+            vito.registrarVehiculo("GPL098", "Azul", "Chiron", "");
+            vincent.registrarRuta("El teatro", new Tuple<double, double>(-2,2.5), new Tuple<double, double>(2,2), new System.Collections.Generic.List<Tuple<double, double>>() { new Tuple<double, double>(2,2.5) }, "Buena la Obra, triste el final");
 
         }
 
@@ -129,6 +130,9 @@ namespace PruebaUnitariaModelo
         public void TestRecomendarRecorridos()
         {
             setup2();
+            mundito.loguearUsuario("3095488541");
+            mundito.recomendarRecorridos(mundito.darUsuario("3095488541").Ubicacion);
+            //Hacer comprobaciones de orden de recorridos, primero vito, segundo michael, tercero vincent
         }
 
         [TestMethod]
